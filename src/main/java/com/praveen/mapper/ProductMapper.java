c307bc3ee2c79ffc48cf1f867210e8d19aa12b7d
Praveen <praveen16499@gmail.com>
2024-09-15 23:52:01 +0530

    mapper concept implement to hide entity details

---
+++ src/main/java/com/praveen/mapper/ProductMapper.java
@@ -0,0 +1,25 @@
+package com.praveen.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.praveen.dto.ProductDto;
+import com.praveen.model.Product;
+
+//@Component
+public class ProductMapper {
+
+	public Product toProduct(ProductDto productDto) {
+		Product product = Product.builder().id(productDto.getId()).name(productDto.getName())
+				.description(productDto.getDescription()).price(productDto.getPrice())
+				.quantity(productDto.getQuantity()).build();
+		return product;
+	}
+
+	public ProductDto toProductDto(Product product) {
+		ProductDto productDto = ProductDto.builder().id(product.getId()).name(product.getName())
+				.description(product.getDescription()).price(product.getPrice()).quantity(product.getQuantity())
+				.build();
+		return productDto;
+	}
+
+}
